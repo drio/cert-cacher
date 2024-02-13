ccab62669859329d93b323472d7a174aeb45f361
David Rio Deiros <driodeiros@gmail.com>
2024-02-13 13:15:03 -0500

    iterate on tests

--- cert-catcher_test.go
+++ cert-catcher_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -15,7 +17,11 @@ func (fn *FakeNodeGetter) GetNode(r *http.Request) (string, error) {
 	return fn.NodeName, nil
 }
 
-func Test_Ping(t *testing.T) {
+var server *httptest.Server
+
+func TestMain(m *testing.M) {
+	fmt.Println("Set up stuff for tests here")
+
 	var store Store
 	store = MemStore{}
 
@@ -23,9 +29,14 @@ func Test_Ping(t *testing.T) {
 	ng = &FakeNodeGetter{}
 
 	handler := createHandler(ng, store)
-	server := httptest.NewServer(handler)
+	server = httptest.NewServer(handler)
 	defer server.Close()
 
+	exitVal := m.Run()
+	os.Exit(exitVal)
+}
+
+func Test_Ping(t *testing.T) {
 	resp, err := http.Get(server.URL + "/ping")
 	if err != nil {
 		t.Fatal(err)
@@ -46,4 +57,27 @@ func Test_Ping(t *testing.T) {
 		t.Errorf("Expected response body to be %s, got %s", expected, body)
 	}
 
+}
+
+func Test_Sh(t *testing.T) {
+	resp, err := http.Get(server.URL + "/sh")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	headerVal := resp.Header.Get("Content-Type")
+	expected := "application/x-sh"
+	if headerVal != expected {
+		t.Errorf("incorrect header expected: %s got: %s", expected, headerVal)
+	}
 }
